1ddab5134133946df6c3f22f38643872cea56af3
Benjamin Drucker <bdrucker@terpmail.umd.edu>
2023-09-05 16:41:34 -0400

    fix files

--- main/dataframes.py
+++ main/dataframes.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # read csv's
-yearly_data = pd.read_csv('data/yearly_data_updated_08_23.csv')
-weekly_data = pd.read_csv('data/weekly_data_updated_08_23.csv')
-spread_data = pd.read_csv('data/spreadspoke_scores.csv')
-teams_data = pd.read_csv('data/nfl_teams.csv')
+yearly_data = pd.read_csv('./data/yearly_data_updated_08_23.csv')
+weekly_data = pd.read_csv('./data/weekly_data_updated_08_23.csv')
+spread_data = pd.read_csv('./data/spreadspoke_scores.csv')
+teams_data = pd.read_csv('./data/nfl_teams.csv')
 
 # drop rows where spread_favorite or over_under_line are nan
 spread_data = spread_data.dropna(subset=['spread_favorite', 'over_under_line'])
@@ -12,4 +12,4 @@ spread_data = spread_data.dropna(subset=['spread_favorite', 'over_under_line'])
 year = 2005
 recent_spread_data = spread_data[spread_data['schedule_season'] >= year]
 
-print(len(recent_spread_data))
+print(spread_data)
